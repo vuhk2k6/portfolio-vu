92c90d8d99f16746d6e59cfa398f31c6ee1c6bd2
vuhk2k6 <109848918+vuhk2k6@users.noreply.github.com>
2022-08-18 17:19:24 +0700

    ip

---
+++ assets/main.js
@@ -0,0 +1,4 @@
+const btnMenu = document.querySelector(".nav-menu");
+const navMenu = document.querySelector(".nav__mobile");
+const navMenu1 = document.querySelector(".nav-list");
+const items = document.querySelectorAll(".nav__item-link");
